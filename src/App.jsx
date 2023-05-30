7d358cd93003844c6c20b81a5565cf32c97d7b01
eze stom <ezequielstom@gmail.com>
2023-05-29 22:33:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./Card";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Carga de estudiantes</h1>
+			<form>
+            <label ></label>
+            <input type="text" id="name" />
+         </form>
+			<Card />
+		</div>
+	);
+}
+export default App;
